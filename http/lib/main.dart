6bc51f748ae6c4e034f80585f46624ee73f8536a
T0NY24 <anperezcue@uide.edu.ec>
2024-02-15 23:36:58 -0500

    Boceto1

---
+++ http/lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<dynamic> posts = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getPosts();
+  }
+
+  Future<void> getPosts() async {
+    var url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      setState(() {
+        posts = jsonDecode(response.body).take(5).toList();
+      });
+    } else {
+      throw Exception('Failed to load posts');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+        itemCount: posts.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(posts[index]['title']),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
